385d5b50b5d3df9b7d5866f9ced557f5a60ab042
Yusuf Aji Wibowo <yusuf.wibowo@mekari.com>
2022-08-19 15:26:23 +0700

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,8 @@ source .env
 ssh root@$HOST_IP_ADDRESS<<EOF
     su fi0smith
     cd /home/fi0smith/workspace/makan-apa
-    git pull origin master
+    git pull origin main
+    PATH="/home/fi0smith/.nvm/versions/node/v16.16.0/bin:$PATH"
     npm install
     pm2 restart all
    exit
