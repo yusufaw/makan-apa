e45246cec204a0d12d6dcdcf554b476f72ff4f2f
Yusuf Aji Wibowo <yusufaw@users.noreply.github.com>
2023-11-20 21:39:58 +0700

    Add flag is active (#1)
    * Add flagging food is active
    
    * Add more food
    
    * Rename to getListFood

--- src/store.js
+++ src/store.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 
 const NAMES_PATH = 'food.json';
 
-exports.getListEvent = new Promise((resolve, reject) => {
+exports.getListFood = new Promise((resolve, reject) => {
     fs.readFile(NAMES_PATH, (err, content) => {
         if (err) {
             console.log('Error loading events file:', err);
